05d05fb9de7af9d34303c31c24c06e3584476dd8
Chris Zuber <shgysk8zer0@gmail.com>
2019-05-25 08:17:27 -0700

    Implement service worker & share API

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 import './std-js/deprefixer.js';
 import './std-js/shims.js';
+import './share-button.js';
 import {checkPwd} from './have-i-been-pwned.js';
-import {$, ready} from './std-js/functions.js';
+import {$, ready, registerServiceWorker} from './std-js/functions.js';
 import {alert} from './std-js/asyncDialog.js';
+
+if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
+	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
+}
+
+document.documentElement.classList.replace('no-js', 'js');
+document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 
 ready().then(() => {
 	$('form').submit(async event => {
@@ -14,6 +22,3 @@ ready().then(() => {
 		alert(`Your password was found ${matchCount} times.`);
 	});
 });
-
-document.documentElement.classList.replace('no-js', 'js');
-document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
